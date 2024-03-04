55fcd251257a3c5d98166ebdfb24db59b9d57e46
jon-jc <jonathancho.jc@gmail.com>
2024-03-03 17:13:10 -0800

    Added header, emerald color theme

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,5 +1,17 @@
-import Image from "next/image";
+import Header from "@/components/Header";
 
 export default function Home() {
-  return <div className="text-green-500">Main Section</div>;
+  return (
+    <div
+      className="
+    bg-neutral-900
+    rounded-lg
+    h-full
+    overflow-hidden
+    overflow-y-auto  
+  "
+    >
+      <Header>Header</Header>
+    </div>
+  );
 }
